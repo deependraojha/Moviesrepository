b272f3b54f9f09439a214be8388e0274470fc545
vagrant <vagrant@kellogg-dev-box>
2014-04-15 13:43:12 +0000

    Generate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Movies::Application.config.secret_key_base = 'b7ac11e0f500ded84cd167ec82af6de020590e978f91f9aa6d6ffeb2700e89b092106892f6686271b7c0233c3d2b7d3e4cbd89c7a918c7505003023d16ddb346'
